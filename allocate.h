cf27fc71c5270361bd0b54a2af2868d0fc8e8399
Brett Hokr <brett.hokr@gmail.com>
2016-01-26 15:40:05 -0600

    Updated Code

--- allocate.h
+++
@@ -1,7 +0,0 @@
-#ifndef ALLOCATE_H
-#define ALLOCATE_H
-
-/* Function definitions */
-void *allocate( unsigned int size );
-
-#endif
--- mcmlModule.h
+++
@@ -1,6 +0,0 @@
-#ifndef MCMLMODULE_H
-#define MCMLMODULE_H
-
-int parseInputString(const char* cfgString, PROP *cfg);
-
-#endif
