6dcfe926bf8a005cad38d21499a21741ec901bec
hokrbh <brett.hokr@gmail.com>
2016-03-24 16:37:23 -0500

    Added validate and moved functions to their own file

--- example.py
+++ example.py
@@ -1,141 +1,101 @@
 #/usr/lib/python3.5
 #==================================================
-#import future # Uncomment this line for python2 (untested)
-import sys
-import struct
-import pMonteCarlo
+import pMonteCarlo as pmc
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-#==================== extra Functions ====================
-def input_det_bin(detDataFilename): 
-    dt=np.dtype([('x-pos','f8'),('y-pos','f8'),('x-vel','f8'),\
-                     ('y-vel','f8'),('z-vel','f8'),('weight','f8'),\
-                     ('time','f8'),('det flag','i'),('seed1','uint32'),\
-                     ('seed2','uint32'),('seed3','uint32'),\
-                     ('seed4','uint32'),('pad','4V')])
-    records=np.fromfile(detDataFilename,dt)
-    return pd.DataFrame(records).drop('pad',axis=1)
-
-def input_abs_bin(absDataFilename,grid_shape): # faster than ascii
-    records=np.fromfile(absDataFilename)
-    return pd.Panel(records.reshape(grid_shape))
-
-def input_det_ascii(detDataFilename):
-    '''Import detData (return as a pd.DataFrame)'''
-    pheader=['x-pos','y-pos','x-vel','y-vel','z-vel','weight',\
-             'time','det flag','seed1','seed2','seed3','seed4']
-    return pd.read_csv(detDataFilename,sep='\t',header=None,\
-                       names=pheader)
-
-def input_abs_ascii(absDataFilename,grid_shape): # slower than bin
-    '''Import absData (return as a pd.Panel)'''
-    idx=pd.MultiIndex.from_product([np.arange(grid_shape[0]),\
-                                   np.arange(grid_shape[1])])
-    df2=pd.read_csv(absDataFilename,sep='\t',header=None).dropna(axis=1) 
-    df2.set_index(idx,inplace=True)                                      
-    return pd.DataFrame.to_panel(df2).transpose(1,2,0)
-
-def angular_diff(data, numAngleBins, numPhotons): 
-    # Build a histogram of the angles of reflection photons
-    numAngleBins = 30 # Bins are assumed to go from 0 to pi/2
-    dalpha = np.pi/(2.0*numAngleBins) # Bin width of the angles
-    dOmega = np.empty(numAngleBins)
-    for i in range(0,numAngleBins):
-        dOmega[i] = 4.0*np.pi*np.sin((i+0.5)*dalpha)*np.sin(dalpha*0.5)
-    Angles = np.arccos(np.fabs(data['z-vel'].values)) # Angles of Ref photons
-    Weights = data['weight'].values
-    AngleHist, AngleBins = np.histogram(Angles, bins=numAngleBins, weights=Weights)
-    AngleHist_psr = np.divide(AngleHist, dOmega)/numPhotons
-    AngleBins_centers = (AngleBins[0:numAngleBins]+0.5*dalpha)/np.pi
-    return AngleHist_psr, AngleBins_centers
     
-#========================= MAIN =========================
-def main():
-    writeDetData = 1 # 0 for no output, 1 for binary, 2 for ascii
-    detDataFilename = 'detData.pmc'
-    numPhotons = 1000000
-    globalSeed = 1
-    backgroundIndex = 1.0
-    layer_leftZ_mm = [0.0]
-    layer_rightZ_mm = [0.2]
-    layer_index = [1.0]
-    layer_g = [0.75]#[0.6]
-    layer_us_permm = [9.0]#[2083.0]#For BaSO4
-    layer_ua_permm = [1.0]#[0.01]
-    log_abs_profile = 0 # 0 for no logging, 1 for binary, 2 for ascii
-    absDataFilename = 'absData.pmc'
-    grid_x_min_mm = -1.0
-    grid_x_max_mm = 1.0
-    grid_x_n = 300
-    grid_y_min_mm = -1.0
-    grid_y_max_mm = 1.0
-    grid_y_n = 400
-    grid_z_min_mm = 0.0
-    grid_z_max_mm = 0.2
-    grid_z_n = 50
+#========== Set up parameters to pass to MCML ==========
+# This is the example from Wang1995 from Tab. 1 and Fig. 3
+writeDetData = 1 # 0 for no output, 1 for binary, 2 for ascii
+detDataFilename = 'detData.pmc'
+numPhotons = 10000
+globalSeed = 1
+backgroundIndex = 1.0
+layer_leftZ_mm = [0.0]
+layer_rightZ_mm = [5.0]
+layer_index = [1.6]
+layer_g = [0.6]
+layer_us_permm = [2083.0]
+layer_ua_permm = [0.001]
+log_abs_profile = 0 # 0 for no logging, 1 for binary, 2 for ascii
+absDataFilename = 'absData.pmc'
+grid_x_min_mm = -1.0
+grid_x_max_mm = 1.0
+grid_x_n = 300
+grid_y_min_mm = -1.0
+grid_y_max_mm = 1.0
+grid_y_n = 400
+grid_z_min_mm = 0.0
+grid_z_max_mm = 0.2
+grid_z_n = 50
 
-    paramList=[numPhotons, globalSeed, backgroundIndex, layer_leftZ_mm,\
-               layer_rightZ_mm, layer_index, layer_g, layer_us_permm, \
-               layer_ua_permm, writeDetData, detDataFilename, \
-               log_abs_profile, absDataFilename, grid_x_min_mm, \
-               grid_x_max_mm, grid_x_n, grid_y_min_mm, grid_y_max_mm, \
-               grid_y_n, grid_z_min_mm, grid_z_max_mm, grid_z_n]
-    paramString=','.join(str(x) for x in paramList) # see Notes
+paramList=[numPhotons, globalSeed, backgroundIndex, layer_leftZ_mm,\
+           layer_rightZ_mm, layer_index, layer_g, layer_us_permm, \
+           layer_ua_permm, writeDetData, detDataFilename, \
+           log_abs_profile, absDataFilename, grid_x_min_mm, \
+           grid_x_max_mm, grid_x_n, grid_y_min_mm, grid_y_max_mm, \
+           grid_y_n, grid_z_min_mm, grid_z_max_mm, grid_z_n]
+paramString=','.join(str(x) for x in paramList) # see Notes
 
-    print(pMonteCarlo.mcml(paramString)) # Run MCML Code
+#========== Run MCML Code ==========
+pmc.mcml(paramString)
 
-    #========== Read in detData ==========
-    if writeDetData != 0:
-        pdata=np.nan
-        if writeDetData == 1:
-            pdata=input_det_bin(detDataFilename)
-        elif writeDetData == 2:
-            pdata=input_det_ascii(detDataFilename)
+#========== Read in detData ==========
+if writeDetData != 0:
+    pdata=np.nan
+    if writeDetData == 1:
+        pdata=pmc.input_det_bin(detDataFilename)
+    elif writeDetData == 2:
+        pdata=pmc.input_det_ascii(detDataFilename)
 
-    #========== Read in absData ==========
-    X={'z':np.linspace(grid_z_min_mm,grid_z_max_mm,grid_z_n),\
-       'y':np.linspace(grid_y_min_mm,grid_y_max_mm,grid_y_n),\
-       'x':np.linspace(grid_x_min_mm,grid_x_max_mm,grid_x_n)}
-    gdata=np.nan
-    if log_abs_profile != 0:
-        grid_shape=(grid_z_n,grid_y_n,grid_x_n)
-        if log_abs_profile == 1:
-            gdata=input_abs_bin(absDataFilename,grid_shape)
-        elif log_abs_profile == 2:
-            gdata=input_abs_ascii(absDataFilename,grid_shape)
+#========== Read in absData ==========
+X={'z':np.linspace(grid_z_min_mm,grid_z_max_mm,grid_z_n),\
+   'y':np.linspace(grid_y_min_mm,grid_y_max_mm,grid_y_n),\
+   'x':np.linspace(grid_x_min_mm,grid_x_max_mm,grid_x_n)}
+gdata=np.nan
+if log_abs_profile != 0:
+    grid_shape=(grid_z_n,grid_y_n,grid_x_n)
+    if log_abs_profile == 1:
+        gdata=pmc.input_abs_bin(absDataFilename,grid_shape)
+    elif log_abs_profile == 2:
+        gdata=pmc.input_abs_ascii(absDataFilename,grid_shape)
 
-    #========== Seperate out reflection from transmission ==========
-    dfr=pdata.drop(pdata[pdata['det flag']!=1].index) # Ref photons
-    dft=pdata.drop(pdata[pdata['det flag']!=2].index) # Trans photons
+#========== Seperate out reflection from transmission ==========
+dfr=pdata.drop(pdata[pdata['det flag']!=1].index) # Ref photons
+dft=pdata.drop(pdata[pdata['det flag']!=2].index) # Trans photons
+dfs=pdata.drop(pdata[pdata['det flag']!=3].index) # Specular photons
 
-    #========== Plot angular diffuse reflectance ==========
-    refAngleHist_psr, refAngleBins_centers = angular_diff(dfr, 30, numPhotons) 
-    plt.figure(1)
-    plt.plot(refAngleBins_centers, refAngleHist_psr, 'ro')
-    plt.xlabel(r"$\pi$ radians")
-    plt.ylabel(r"$R ( \theta ) sr^{-1}$")
+#========== Compute total reflection and transmission ==========
+diffRef=sum(dfr['weight'].values)/numPhotons
+specRef=sum(dfs['weight'].values)/numPhotons
+totalRef=diffRef+specRef
+print('Total reflection coefficient:', totalRef)
+totalTrans=sum(dft['weight'].values)/numPhotons
+print('Total transmission coefficient:', totalTrans)
 
-    #========== Plot angular diffuse transmission ==========
-    transAngleHist_psr, transAngleBins_centers = angular_diff(dft, 30, numPhotons) 
-    plt.figure(2)
-    plt.plot(transAngleBins_centers, transAngleHist_psr, 'ro')
-    plt.xlabel(r"$\pi$ radians")
-    plt.ylabel(r"$T ( \theta ) sr^{-1}$")
+#========== Plot angular diffuse reflectance ==========
+refAngleHist_psr, refAngleBins_rad = pmc.angular_diff(dfr, 30, numPhotons) 
+plt.figure(1)
+plt.plot(refAngleBins_rad, refAngleHist_psr, 'ro')
+plt.xlabel(r"$\pi$ radians")
+plt.ylabel(r"$R ( \theta )$ sr$^{-1}$")
 
-    plt.show()
+#========== Plot angular diffuse transmission ==========
+transAngleHist_psr, transAngleBins_rad = pmc.angular_diff(dft, 30, numPhotons) 
+plt.figure(2)
+plt.plot(transAngleBins_rad, transAngleHist_psr, 'ro')
+plt.xlabel(r"$\pi$ radians")
+plt.ylabel(r"$T ( \theta )$ sr$^{-1}$")
 
-    return pdata,gdata,X
+#========== Plot temporal reflectance ==========
+refTimeHist_perps, refTimeBins_ps = pmc.temporal_diff(dfr, 0.0, 1000.0, 30, numPhotons)
+plt.figure(3)
+plt.semilogy(refTimeBins_ps, refTimeHist_perps, 'ro')
+plt.xlabel(r"$t$ (ps)")
+plt.ylabel(r"$R ( t )$ ps$^{-1}$")
 
-#==================================================
-if __name__ == '__main__':
-    pdata,gdata,X=main()
-    #pMatrix=pdata.values# Use this to convert DataFrame to numpy Array
-
-    dfr=pdata.drop(pdata[pdata['det flag']!=1].index) # Ref photons
-    dft=pdata.drop(pdata[pdata['det flag']!=2].index) # Trans photons
-    dfa=pdata.drop(pdata[pdata['det flag']!=4].index) # absorb photons 
+plt.show()
 
     #pdata[pdata['det flag']==1]['time'].hist(bins=100, weights=pdata[pdata['det flag']==1]['weight'], range=(0.0, 10.0))#Hist of time
     
